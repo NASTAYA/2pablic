0a6f5db3f5e8553d97f3f196cd72c3cf83452e5e
Анастасия Мательская <nastaya1974@gmail.com>
2022-01-07 11:38:58 +0300

    Задание 16.7.1

---
+++ TestCat.py
@@ -0,0 +1,24 @@
+import json
+
+from Cats import Cat
+
+with open('Pet_List.json', encoding='utf8') as f:
+    templates = json.load(f)
+
+print(templates)
+
+
+def find_cat(item, el):
+    if item[el]['code'] == 'cat':
+        return True
+
+
+for i in templates['results']:
+    for j in i:
+        if i == 'species' and find_cat(i, j):
+            obj_cat = Cat(i['name'], i['gender']['name'], i['age'])
+            print(obj_cat.name)
+            print(obj_cat.gender)
+            print(obj_cat.age)
+        else:
+            print(False)
